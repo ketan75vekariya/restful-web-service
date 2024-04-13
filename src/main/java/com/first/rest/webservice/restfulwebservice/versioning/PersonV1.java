6c0da1f11527305b1254a6e2da624ef8ef91eec3
ketan75vekariya <ketan75vekariya@yahoo.com>
2024-04-13 13:44:08 +0100

    Explored versioning of API

---
+++ src/main/java/com/first/rest/webservice/restfulwebservice/versioning/PersonV1.java
@@ -0,0 +1,19 @@
+package com.first.rest.webservice.restfulwebservice.versioning;
+
+public class PersonV1 {
+	private String name;
+
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonV1 [name=" + name + "]";
+	}
+}
---
+++ src/main/java/com/first/rest/webservice/restfulwebservice/versioning/VersioningPersonController.java
@@ -0,0 +1,17 @@
+package com.first.rest.webservice.restfulwebservice.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonController {
+	@GetMapping("/V1/person")
+	public PersonV1 getFirstVersionOfPerson() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping("/V2/person")
+	public PersonV2 getSecondVersionOfPerson() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+}
