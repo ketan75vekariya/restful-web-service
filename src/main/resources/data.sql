e258f2c392f149ac98059d2e5e927f69f7eb33fa
ketan75vekariya <ketan75vekariya@yahoo.com>
2024-04-14 14:20:54 +0100

    Created Database and add values in it

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details (id, birthdate,name) values(10001, current_date(),'Prisha');
+insert into user_details (id, birthdate,name) values(10002, current_date(),'Ketan');
+insert into user_details (id, birthdate,name) values(10003, current_date(),'Ekta');
